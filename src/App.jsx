7e4b71124cb0c869bdf5312dc92affbd85e47186
Akash Pandya <akash.pandya@mindinventory.com>
2023-01-05 19:09:42 +0530

    test refs

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Main from "./Test/Main";
 
 const App = () => {
   return (
-    <div className='min-h-screen flex flex-col justify-start items-center p-2 bg-gradient-to-r from-teal-500 to-cyan-500'>
+    <div className='font-roboto min-h-screen flex flex-col justify-start items-center p-2 bg-gradient-to-r from-teal-500 to-cyan-500'>
       {/* <Main /> */}
       <MainPage />
     </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ['"Roboto"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [require("tailwind-scrollbar-hide")],
 };
