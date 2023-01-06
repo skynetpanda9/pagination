95b30265f8628e785f824233fee42654b38ef4e8
Akash Pandya <akash.pandya@mindinventory.com>
2023-01-06 15:02:59 +0530

    basic cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import MainPage from "./Components/MainPage";
-import Main from "./Test/Main";
 
 const App = () => {
   return (
     <div className='font-roboto min-h-screen flex flex-col justify-start items-center p-2 bg-gradient-to-r from-teal-500 to-cyan-500'>
-      {/* <Main /> */}
       <MainPage />
     </div>
   );
