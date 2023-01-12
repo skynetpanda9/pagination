00215c18054fa3658f33a6e07a8272985a258610
Akash Pandya <akash.pandya@mindinventory.com>
2023-01-12 16:18:13 +0530

    cleanup paginate final ...

--- src/Utils/usePaginate.js
+++ src/Utils/usePaginate.js
@@ -1,13 +1,11 @@
 import { useMemo } from "react";
 
-// export const DOTS = "...";
-
-const range = (start, end) => {
-  let length = end - start + 1;
-  return Array.from({ length }, (_, idx) => idx + start);
-};
-
 export const usePagination = ({ totalCount, pageSize, currentPage }) => {
+  const range = (start, end) => {
+    let length = end - start + 1;
+    return Array.from({ length }, (_, idx) => idx + start);
+  };
+
   const paginationRange = useMemo(() => {
     const totalPageCount = Math.ceil(totalCount / pageSize); //20 (when items/page = 10) (200/10)
 
